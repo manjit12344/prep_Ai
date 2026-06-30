3fa69e8e079bedac086547d4e45eb1745c833ec9
manjit12344 <checkforadmin@gmail.com>
2026-06-30 19:16:47 +0530

    ai change

--- backend/src/services/05.deepseek.service.js
+++ backend/src/services/05.deepseek.service.js
@@ -12,7 +12,7 @@
     if(check[check.length-1] === "pdf")  isPdf = true;
     const stream = await openrouter.chat.send({
       chatRequest: {
-      model: "openrouter/owl-alpha",
+      model: "openrouter/free",
       response_format: { type: "json_object" },
       stream: false,
       messages: [
